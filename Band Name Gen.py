78cdd9ffc82ed5e5335be4e7bfa4cb464ad1a567
milktoast <paperotavio@gmail.com>
2025-06-14 09:04:35 -0300

    Add files via upload

---
+++ Band Name Gen.py
@@ -0,0 +1,22 @@
+# Step 0: Screwing around
+input("Hey there, user. What's your name?\n")
+# This one's just for funsies. I was testing out the line break to see if it'd work.
+#
+# Step 1: Programming Greeting
+print("Sorry Chuddy, I don't care. Welcome to my band name generator!")
+#
+# Step 2: Ask user about their city
+city = input("So uhh, what city did you grow up in?\n")
+# Create a variable here and assign input to so that this data doesn't go anywhere
+# and I can print it or use it later on!
+#
+# Step 3: Ask user about their pet
+petname = input("nice, nice. Tell me one of your pets' names now\n")
+# Basically going to repeat step 2/line 4.
+#
+# Step 4: Combine the names now
+print("Welp, guess we could go with " + city + " " + petname + " luhmao")
+# Number of ways to do this step, but I will go with what I learned.
+# Got a bit of string concatenation here. Successful all around! Didn't even need the hints!
+# Runs just like the demo. I was able to apply everything I learned today.
+# Printing, commenting, debugging, string manipulation, and variables! Solid course for day 1.
