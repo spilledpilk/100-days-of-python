ef4f9bd6f794494122a748384e2eb7764ff74bb0
milktoast <paperotavio@gmail.com>
2025-06-15 02:56:17 -0300

    Create Data Types.py

---
+++ Data Types.py
@@ -0,0 +1,29 @@
+# Using 0 will bring me the first character of this string. Programmers start counting from 0.
+# This method of pulling out a particular element from a string is called subscripting.
+# Number between the square brackets determines which character we're pulling out.
+# Important to note when we use this to make sure we're counting from 0 or from 1.
+print("Hello"[-1])
+# String
+print("123" + "345") # This will merely concatenate like "Hello " and "world"
+# This will yield 123345
+
+# Integer = Whole numbers without decimal places
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+## Float = Floating Point Number
+print(3.14159)
+# This because it has a decimal place is now a float data type.
+# If you think about the decimal point as being able to float around the number
+# because it can occur at any point, you have a floating point number.
+
+# Boolean
+# Boolean
+print(True)
+print(False)
+# Note these values are capitalized.
+# Basically going to be using this A LOT. Always start with capital T or F.
+# They have no quotation marks or anything. This is used for your program to
+# test if something's true or false and respond accordingly.
