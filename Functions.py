5886f3e94647e362b49cf05cce0777a732b5ee7f
milktoast <paperotavio@gmail.com>
2025-06-19 05:00:18 -0300

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,26 @@
+# num_char = len("Hello")
+# print(len("Hello"))
+def my_function():
+#    print("Hello")
+    a=3
+    if a >2:
+        print("This will run")
+print("Bye")
+
+# What if we wanted to make our own functions? How would we do that?
+# First start out with our keyword def.
+# def => define a keyword.
+# Syntax details: def #your function name#():
+# def my_function():
+#     # Hit enter and you'll get an indentation. Everything below the line is a code block that defines what this function does.
+#     print("Hello")
+#     print("Bye")
+#
+# If you were to just run your code, nothing will happen.
+# The reason for it is that we haven't yet executed the function.
+# It's only when we actually need it that we'll trigger it.
+# We have to call the function.
+# Type the name of the function and add any necessary inputs.
+# In our case, we don't need any.
+# To do this, just type out
+my_function()
